0b821cbcb7f2fd2ad92576813b0b18654876b402
Wampirok <marek.waclawik@onet.pl>
2023-05-13 11:26:23 +0200

    calculator fix

--- src/main/java/pl/com/sda/zdjavapol141/Calculator.java
+++ src/main/java/pl/com/sda/zdjavapol141/Calculator.java
@@ -1,8 +1,8 @@
 package pl.com.sda.zdjavapol141;
 
 public class Calculator {
-    public int calculate(int a, int b){
-        int c=a+b;
+    public int calculate(int a, int b) {
+        int c = a + b;
         return c;
     }
 }
